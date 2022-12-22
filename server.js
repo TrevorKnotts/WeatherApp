04ff039d06e527d7f6c95bf6c4593f3e73d57a2e
Knotts <tknotts@wkhs.com>
2022-12-22 16:05:48 -0600

    First change.

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
+const request = require('request');
+//var http = require('http');
+//var port = process.env.PORT || 1337;
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+//http.createServer(function (req, res) {
+//    res.writeHead(200, { 'Content-Type': 'text/plain' });
+//    res.end('Hello World\n');
+//}).listen(port);
+
+const url = 'http://api.weatherstack.com/current?access_key=13e065c67ccf7bc946126e9ac5502273&query=37.8267,-122.4233&units=f'
+
+request({ url: url, json: true }, (error, response) => {
+    const data = response.body.current;
+    const temp = data.temperature;
+    const feel = data.feelslike;
+    console.log('It is currently ' + temp + ' degrees out. It feels like ' + feel + ' degrees out.');
+    
+});
