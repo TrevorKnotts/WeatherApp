8a763e9440e5895b247191509dfefa802ac459e3
Knotts <tknotts@wkhs.com>
2022-12-29 11:53:52 -0600

    Ep. 37 Callback Abstraction Challenge

--- server.js
+++ server.js
@@ -1,31 +1,13 @@
 'use strict';
 const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
-//var http = require('http');
-//var port = process.env.PORT || 1337;
-
-//http.createServer(function (req, res) {
-//    res.writeHead(200, { 'Content-Type': 'text/plain' });
-//    res.end('Hello World\n');
-//}).listen(port);
-
-//const url = 'http://api.weatherstack.com/current?access_key=13e065c67ccf7bc946126e9ac5502273&query=37.8267,-122.4233&units=f'
-
-//request({ url: url, json: true }, (error, response) => {
-//    if (error) {
-//        console.log('Error: ' + error);
-//    } else if (response.body.error) {
-//        console.log('Unable to find location.');
-//    } else {
-//        const data = response.body.current;
-//        const temp = data.temperature;
-//        const feel = data.feelslike;
-//        console.log('It is currently ' + temp + ' degrees out. It feels like ' + feel + ' degrees out.');
-//    }
+//geocode('Boston', (error, data) => {
+//    console.log('Error: ', error);
+//    console.log('Data: ', data);
 //});
 
-
-geocode('Boston', (error, data) => {
+forecast(-75.7088, 44.1545, (error, data) => {
     console.log('Error: ', error);
     console.log('Data: ', data);
 });
---
+++ utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request');
+
+const forecast = (lat, lon, callback) => {
+    const weatherUrl = 'http://api.weatherstack.com/current?access_key=13e065c67ccf7bc946126e9ac5502273&query=' +
+        lat + ',' + lon + '&units=f';
+    request({ url: weatherUrl, json: true }, (error, response) => {        
+        if (error) {
+            callback(('Error: ' + error), undefined);
+        } else if (response.body.error) {
+            callback('Unable to find location.', undefined);
+        } else {
+            const data = response.body.current;
+            const temp = data.temperature;
+            const feel = data.feelslike;
+            callback(undefined, 'It is currently ' + temp + ' degrees out. It feels like ' + feel + ' degrees out.');
+        }
+    });
+};
+
+module.exports = forecast;
