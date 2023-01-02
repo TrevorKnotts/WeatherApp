77d54fef366f796c6ff7eb85076df7f5cbc5c62b
Trevor Knotts <Trevoraknotts@gmail.com>
2023-01-01 19:59:34 -0600

    Ep. 38: Callback Chaining

--- server.js
+++ server.js
@@ -2,12 +2,19 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-//geocode('Boston', (error, data) => {
-//    console.log('Error: ', error);
-//    console.log('Data: ', data);
-//});
+const loc = process.argv[2];
 
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error: ', error);
-    console.log('Data: ', data);
+geocode(loc, (error, data) => {
+    if (error) {
+        return console.log(error);
+    }
+
+   forecast(data.latitude, data.longitude, (error, forecastData) => {
+        if (error) {
+            return console.log(error);
+        }
+
+        console.log(data.location);
+        console.log(forecastData);
+    });
 });
--- utils/forecast.js
+++ utils/forecast.js
@@ -1,13 +1,12 @@
 const request = require('request');
 
 const forecast = (lat, lon, callback) => {
-    const weatherUrl = 'http://api.weatherstack.com/current?access_key=13e065c67ccf7bc946126e9ac5502273&query=' +
-        lat + ',' + lon + '&units=f';
+    const weatherUrl = 'http://api.weatherstack.com/current?access_key=13e065c67ccf7bc946126e9ac5502273&query=' + lat + ',' + lon + '&units=f';
     request({ url: weatherUrl, json: true }, (error, response) => {        
         if (error) {
             callback(('Error: ' + error), undefined);
         } else if (response.body.error) {
-            callback('Unable to find location.', undefined);
+            callback(response.body.error, undefined);
         } else {
             const data = response.body.current;
             const temp = data.temperature;
